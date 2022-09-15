0f9226835acdb0209317ff38dd424fdcd50a7706
Remix Workspace <>
2022-09-15 13:21:37 +0300

    Initial commit

---
+++ contracts/MyContract.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+contract MyContract {
+    string public ourString = "Hello World";
+
+    function updateOurString(string memory _updatedString) public {
+        ourString = _updatedString;
+    }
+}
